7fa312e812cad3e979930f123cd295013d865f09
ChintanD2205 <chintan222005@gmail.com>
2025-11-19 22:37:03 +0530

    Added backend structure and mongo connect

---
+++ backend/config/database.js
@@ -0,0 +1,24 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+const connectDB = async () => {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
